6d74d139121aac8a2a13fa7fb32fd47c431f302d
ts030930 <ts930@dgu.ac.kr>
2026-03-08 16:11:59 +0900

    first disk

--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,7 @@
 #include "idt/idt.h"
 #include "memory/heap/kheap.h"
 #include "memory/paging/paging.h"
+#include "disk/disk.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -89,20 +90,11 @@ void kernel_main()
     // Switch to kernel paging chunk
     paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
 
-    char* ptr = kzalloc(4096);
-    paging_set(paging_4gb_chunk_get_directory(kernel_chunk),(void*)0x1000,(uint32_t)ptr|PAGING_ACCESS_FROM_ALL|PAGING_IS_PRESENT|PAGING_IS_WRITEABLE);
-
-      char* ptr2 = (char*) 0x1000;
-    ptr2[0] = 'A';
-    ptr2[1] = 'B';
-    print(ptr2);
-
-    print(ptr);
-
-
-
     // Enable paging
     enable_paging();
+
+    char buf[512];
+    disk_read_sector(0,1,buf);
 
     // Enable Interrupts
     enable_interrupts();
