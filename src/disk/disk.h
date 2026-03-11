b7bb4169168b6e39c819486d5423831cc53c9b45
ts030930 <ts930@dgu.ac.kr>
2026-03-11 18:00:17 +0900

    implement basic VFS

--- src/disk/disk.h
+++ src/disk/disk.h
@@ -1,6 +1,7 @@
 #ifndef DISK_H
 #define DISK_H
 
+#include "fs/file.h"
 typedef unsigned int PEACHOS_DISK_TYPE;
 
 #define PEACHOS_DISK_TYPE_REAL 0
@@ -9,7 +10,7 @@ struct disk
 {
     PEACHOS_DISK_TYPE type;
     int sector_size;
-
+    struct filesystem* filesystem;
 };
 
 void disk_search_and_init();
